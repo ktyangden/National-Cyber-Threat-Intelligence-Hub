5b681fe18d139ea8ebf5194fdba629cd544bae2b
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-11-20 22:47:16 +0530

    added about story

--- Frontend/src/pages/About/AboutStory.tsx
+++ Frontend/src/pages/About/AboutStory.tsx
@@ -1,5 +1,26 @@
 export default function AboutStory() {
     return (
-        <div>AboutStory</div>
+        <section id="about-story" className="overflow-hidden text-neutral-900 dark:text-neutral-100 h-240 mt-15">
+            <div className="flex w-full h-full justify-center items-center py-25 overflow-hidden">
+                <div className="flex w-1/2 h-full ml-65 justify-center items-center px-15">
+                    <p className="text-2xl">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.<br/><br/> 
+                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec 
+                        quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede 
+                        justo, fringilla vel, aliquet nec, vulputate
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.<br/><br/> 
+                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec 
+                        quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede 
+                        justo, fringilla vel, aliquet nec, vulputate
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.<br/><br/> 
+                        Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec 
+                        quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+                    </p>
+                </div>
+                <div className="flex-col w-1/2 h-full mr-65">
+                    <div className="w-4/5 h-2/3 bg-[#F2613F] rounded-3xl z-0"></div>
+                    <div className="w-4/5 h-2/3 bg-[#9B3922] rounded-3xl z-1 -translate-y-63 translate-x-40"></div>
+                </div>
+            </div>
+        </section>
     )
 }
