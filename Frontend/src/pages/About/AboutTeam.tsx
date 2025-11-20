552fb0853a02306616d682915041d91045ef6f8b
Kanishak-xd <kanishak.sharma22@st.niituniversity.in>
2025-11-21 00:45:11 +0530

    added about team

--- Frontend/src/pages/About/AboutTeam.tsx
+++ Frontend/src/pages/About/AboutTeam.tsx
@@ -1,5 +1,34 @@
+import ProfileCard from "@/components/ProfileCard";
+
 export default function AboutTeam() {
-    return (
-        <div>AboutTeam</div>
-    )
+  return (
+    <section id="about-team" className="overflow-hidden text-neutral-900 dark:text-neutral-100 my-15 flex flex-col items-center">
+        <div className="w-full flex justify-center items-center">
+            <h1 className="text-7xl mt-12 font-semibold">Our Team</h1>
+        </div>
+
+        <div className="w-full flex py-20 justify-center">
+            <div className="max-w-7xl w-full flex flex-wrap items-center justify-center gap-10">
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/151437546?v=4" 
+                    profileLink="https://github.com/Kanishak-xd" profileName="Kanishak Sharma"
+                />
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/78788104?v=4" 
+                    profileLink="https://github.com/ktyangden" profileName="Karma Yangden"
+                />
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/128486662?v=4" 
+                    profileLink="https://github.com/VK-627" profileName="Vishwas Kaushik"
+                />
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/194140611?v=4" 
+                    profileLink="https://github.com/mohitkhandal22" profileName="Mohit Khindal"
+                />
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/190525789?v=4" 
+                    profileLink="https://github.com/Harshwardhan199" profileName="Harshwardhan Saini"
+                />
+                <ProfileCard profileImg="https://avatars.githubusercontent.com/u/136967302?v=4" 
+                    profileLink="https://github.com/rakshitsawarn" profileName="Rakshit Sawarn"
+                />
+            </div>
+        </div>    
+    </section>
+  );
 }
