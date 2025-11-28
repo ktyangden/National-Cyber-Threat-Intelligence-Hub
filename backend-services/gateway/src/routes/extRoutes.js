693483b54b08f3738b6f94f93400ab17e5709aad
mohitkhandal22 <khandalmohit9588@gmail.com>
2025-11-28 23:03:08 +0530

    .yaml modified to use dockerhub for images

--- backend-services/gateway/src/routes/extRoutes.js
+++ backend-services/gateway/src/routes/extRoutes.js
@@ -22,7 +22,7 @@ router.get("/logs/recent-logs", async (req, res) => {
 
 router.get("/logs/persistent-stats", async (req, res) => {
   try {
-    const response = await axios.get(`http://localhost:8001/persistent-stats`, {
+    const response = await axios.get(`http://log-service:8001/persistent-stats`, {
       headers: { Accept: "application/json" }
     });
 
